2fbf42e4b1c7e4f5e506518c84d170070e916c50
techgineer <ahdavis13@gmail.com>
2017-04-09 17:22:53 -0500

    Added main code file

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * main.cpp
+ * Runs main code for Orbis
+ * Created by Andrew Davis
+ * Created on 4/9/2017
+ * Open source (GPL license)
+ */
+
+//includes
+#include "core/Game.h"
+#include <iostream>
+#include <cstdlib>
+
+//main function - main entry point for program
+int main(int argc, char* argv[]) {
+	//start by calling init()
+	if(!Game::start()) { //if the game failed to start
+		std::cerr << "Fatal: Failed to start game." << std::endl; //display an error message
+		return EXIT_FAILURE; //and exit with an error
+	} else { //if the init succeeded
+		Game::run(); //run the game
+	}
+	Game::shutdown(); //shut the game down
+	return EXIT_SUCCESS; //and exit with no errors
+}
+
+//end of program
