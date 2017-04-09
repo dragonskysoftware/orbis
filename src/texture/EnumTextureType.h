c598c1f43ca19fa5e3980027f806432906633f1c
techgineer <ahdavis13@gmail.com>
2017-04-09 17:23:44 -0500

    Added texturing code

---
+++ src/texture/EnumTextureType.h
@@ -0,0 +1,19 @@
+/*
+ * EnumTextureType.h
+ * Enumerates different types of game component textures for Orbis
+ * Created by Andrew Davis
+ * Created on 4/9/2017
+ * Open source (GPL license)
+ */
+
+//disallow reinclusion
+#ifndef ENUM_TEXT_TYPE
+#define ENUM_TEXT_TYPE
+
+//enum definition
+enum class EnumTextureType {
+	TEXT, //text texture
+	IMG, //image texture
+};
+
+#endif
