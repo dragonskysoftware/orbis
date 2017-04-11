af1675cf6a76fd3adf129f7a997c77cf230ff448
techgineer <ahdavis13@gmail.com>
2017-04-11 15:34:45 -0500

    Added splash screen code

--- src/core/Game.h
+++ src/core/Game.h
@@ -16,6 +16,7 @@
 #include "../component/ImgComponent.h"
 #include "../component/TextComponent.h"
 #include "../util/constants.h"
+#include "../util/functions.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
@@ -44,6 +45,9 @@ class Game final {
 
 	//private fields and methods
 	private:
+		//methods
+		static void displaySplash(); //displays the splash image
+
 		//SDL fields
 		static SDL_Window* window; //the window the game runs in
 		static SDL_Renderer* renderer; //the graphics renderer for the game
@@ -51,6 +55,7 @@ class Game final {
 		//game object fields
 		static TextComponent* message; //a temporary message displayed while the game is being developed
 		static ImgComponent* image; //an image used to test graphics rendering
+		static ImgComponent* splash; //the splash image for the game
 
 };
 
