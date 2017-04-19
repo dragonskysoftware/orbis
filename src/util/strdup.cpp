90014f5db5e8f49639bebc52536f3fde42899005
techgineer <ahdavis13@gmail.com>
2017-04-19 12:03:49 -0500

    Added C-style strdup function

--- src/util/strdup.cpp
+++ src/util/strdup.cpp
@@ -1,6 +1,6 @@
 /*
  * strdup.cpp
- * Implements a function that duplicates character strings
+ * Implements functions that duplicate character strings
  * Created by Andrew Davis
  * Created on 4/9/2017
  * All rights reserved
@@ -14,6 +14,16 @@ char* strdup(const char* str) noexcept {
 	size_t len = strlen(str) + 1; //get the length of the supplied string plus one
 	char* ret = new char[len]; //allocate a character string with the calculated length
 	strcpy(ret, str); //copy in the supplied string
+	return ret; //return the duplicated, initialized string
+}
+
+//C_strdup function implementation - duplicates a character string C style
+char* C_strdup(const char* str) noexcept {
+	size_t len = strlen(str) + 1; //get the length of the supplied string plus one
+	char* ret = (char*)SDL_malloc(len * sizeof(char)); //allocate a character string with the calculated length
+	if(ret) { //if the allocation succeeded
+		strcpy(ret, str); //then copy in the supplied string
+	}
 	return ret; //return the duplicated, initialized string
 }
 
--- src/util/strdup.h
+++ src/util/strdup.h
@@ -1,6 +1,6 @@
 /*
  * strdup.h
- * Declares a function that duplicates character strings
+ * Declares functions that duplicate character strings
  * Created by Andrew Davis
  * Created on 4/9/2017
  * All rights reserved
@@ -11,10 +11,16 @@
 #define STRDUP_H_INC
 
 //include
+#include <SDL2/SDL.h>
 #include <cstring>
 #include <cstdlib>
 
-//function declaration
+//function declarations
+
+//strdup function - duplicates a string C++ style
 char* strdup(const char* str) noexcept;
+
+//C_strdup function - duplicates a string C style
+char* C_strdup(const char* str) noexcept;
 
 #endif
